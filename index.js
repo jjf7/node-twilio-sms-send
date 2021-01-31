1d727afbb333ce26b0fa03fe94c43a23759ddd80
jjf7 <jfsistemas.contacto@gmail.com>
2021-01-31 12:23:35 -0400

    upload

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const dotenv = require('dotenv').config();
 const accound_sid = process.env.ACCOUND_SID;
 const auth_token = process.env.AUTH_TOKEN;
 const client = require('twilio')(accound_sid,auth_token);
-
 client.messages
     .create({
         body: 'I love it',
