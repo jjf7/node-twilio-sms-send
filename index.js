ae48aad08003d2188d7979331550e7baaf57b36b
jjf7 <jfsistemas.contacto@gmail.com>
2021-01-31 12:15:04 -0400

    my first upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const dotenv = require('dotenv').config();
+const accound_sid = process.env.ACCOUND_SID;
+const auth_token = process.env.AUTH_TOKEN;
+const client = require('twilio')(accound_sid,auth_token);
+
+client.messages
+    .create({
+        body: 'I love it',
+        from: '+12513166721',
+        to:'+584128256644'
+    }).then(message => console.log(message.sid))
